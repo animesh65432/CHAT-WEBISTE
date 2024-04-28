2fd4516af8c6d8e6b3954d65a9a39c6f28a6a3a9
animesh65432 <kiranduttta234@gmail.com>
2024-04-28 21:43:12 +0530

    .gitignore is now working

--- backend/dist/router/messages/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const messages_1 = require("../../controllers/messages");
-const middlewares_1 = require("../../middlewares");
-const router = (0, express_1.Router)();
-router.post("/Postmessage", middlewares_1.Authentication, messages_1.posthemessage);
-router.get("/GettheMessages", middlewares_1.Authentication, messages_1.GettheMesaage);
-exports.default = router;
